d5701a649a98b27770bbfecc2341f7e951d0cdd8
Xu Wang <xwang9092@gmail.com>
2023-08-24 21:11:09 -0400

    feat: implement transaction service controller

---
+++ src/main/java/betterbanking/TransactionService.java
@@ -0,0 +1,10 @@
+package betterbanking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionService {
+    public List<Transcation> findAllByAccountNumber() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/main/java/betterbanking/Transcation.java
@@ -0,0 +1,11 @@
+package betterbanking;
+
+public class Transcation {
+    String type;
+    String date;
+    String accountNumber;
+    String currency;
+    String amount;
+    String merchantName;
+    String merchantLogo;
+}
