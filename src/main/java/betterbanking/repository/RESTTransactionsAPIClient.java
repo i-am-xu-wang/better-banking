6d13159d8dbc3cc5fce4a2301e96f67437417ea2
Xu Wang <xwang9092@gmail.com>
2023-09-23 19:27:39 -0400

    feat: use user credentials to make post request to testnet for authorization token

--- src/main/java/betterbanking/repository/RESTTransactionsAPIClient.java
+++ src/main/java/betterbanking/repository/RESTTransactionsAPIClient.java
@@ -4,9 +4,13 @@ import betterbanking.Transaction;
 import betterbanking.integration.OBTransactionAdapter;
 import betterbanking.model.OBReadTransaction6;
 import betterbanking.model.OBTransaction6;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Base64Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
@@ -18,6 +22,10 @@ public class RESTTransactionsAPIClient implements TransactionApiClient {
     private WebClient client;
     private OBTransactionAdapter obTransactionAdapter = new OBTransactionAdapter();
 
+    @Value("${testnet.integration.client}")
+    private String clientId;
+    @Value("${testnet.integration.secret}")
+    private String secret;
     @Autowired
     public RESTTransactionsAPIClient(WebClient client) {
         this.client = client;
@@ -25,11 +33,24 @@ public class RESTTransactionsAPIClient implements TransactionApiClient {
 
     public List<Transaction> findAllByAccountNumber(Integer accountNumber) {
         OBReadTransaction6 res = null;
+        String encodedClientCredentials = Base64Util.encode(String.format("%s:%s", clientId, secret));
         try {
-            res = client.get()
-                    .uri("accounts/" + accountNumber + "/transactions")
+            res = client.post()
+                    .uri("/oauth/token")
+                    .header("Authorization", "Basic " + encodedClientCredentials)
+                    .body(BodyInserters.fromFormData("grant_type", "client_credentials"))
                     .retrieve()
-                    .bodyToMono(OBReadTransaction6.class).block();
+                    .bodyToMono(JsonNode.class)
+                    .flatMap(tokenResponse -> {
+                        String accessTokenValue = tokenResponse.get("access_token")
+                                .textValue();
+                        return client.get()
+                                .uri("accounts/" + accountNumber + "/transactions")
+                                .headers(h -> h.setBearerAuth(accessTokenValue))
+                                .retrieve()
+                                .bodyToMono(OBReadTransaction6.class);
+                    })
+                    .block();
         } catch (Exception ex) {
             log.error("failed to retrieve account information due to the following reason {}", ex.getMessage());
         }
