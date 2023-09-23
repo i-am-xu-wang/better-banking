c7b7408fed53fdf06955f0af5074c780387a889f
Xu Wang <xwang9092@gmail.com>
2023-09-22 22:42:38 -0400

    feat: update transactionService to use api client call
    - add circuit breaker and fallback method to transcationService

--- src/main/java/betterbanking/TransactionService.java
+++ src/main/java/betterbanking/TransactionService.java
@@ -1,13 +1,36 @@
 package betterbanking;
+import java.util.ArrayList;
 import java.util.List;
 
+import betterbanking.repository.MerchantDetailsRepository;
+import betterbanking.repository.TransactionApiClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
-    public List<Transaction> findAllByAccountNumber(Integer accountNumber) {
-        return List.of(
-            new Transaction("credit", null, 1234567, "USD", 100.00, "acme", "acme.png")
-        );
+    private final TransactionApiClient transactionApiClient;
+    private final MerchantDetailsRepository merchantDetailsRepository;
+    public TransactionService(
+            final TransactionApiClient transactionApiClient,
+            final MerchantDetailsRepository merchantDetailsRepository) {
+        this.transactionApiClient = transactionApiClient;
+        this.merchantDetailsRepository = merchantDetailsRepository;
+    }
+
+    @CircuitBreaker(name = "transactionService", fallbackMethod = "foundNone")
+    public List<Transaction> findAllByAccountNumber(final Integer accountNumber) {
+        List<Transaction> transactions= transactionApiClient.findAllByAccountNumber(accountNumber);
+        transactions.forEach(transaction -> {
+            merchantDetailsRepository.findMerchantLogo(transaction.getMerchantName()).ifPresent(logo -> {
+                transaction.setMerchantLogo(logo);
+            });
+        });
+
+        return transactions;
+    }
+
+    private List<Transaction> foundNone(final Integer accountNumber, final Throwable throwable) {
+        return new ArrayList<>();
     }
 }
