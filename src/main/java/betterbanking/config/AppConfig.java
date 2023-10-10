52e37d9e8ed78aa3d7c957fdda6c9de93ed4199f
Xu Wang <xwang9092@gmail.com>
2023-10-10 15:45:27 -0400

    feat: connect to redis server and implement cache to save request response

---
+++ src/main/java/betterbanking/config/AppConfig.java
@@ -0,0 +1,30 @@
+package betterbanking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@EnableCaching
+@Configuration
+public class AppConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    private @Value("${spring.redis.host}") String host;
+    private @Value("${spring.redis.port}") int port;
+}
