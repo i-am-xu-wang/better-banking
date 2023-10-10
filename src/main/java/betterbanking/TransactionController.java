6ebebc4339473266424c59f8f5e121d2ae8d8186
Xu Wang <xwang9092@gmail.com>
2023-10-10 10:24:44 -0400

    feat: implement github oauth2 login

--- src/main/java/betterbanking/TransactionController.java
+++ src/main/java/betterbanking/TransactionController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/transactions")
 public class TransactionController {
     private TransactionService transactionService;
